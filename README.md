b3ee1bd455039367c70cdaef5aa37aabf8e0955d
friend <friend@yourfriend.lv>
2021-08-13 23:41:53 +0300

    Add banning

--- README.md
+++ README.md
@@ -6,15 +6,11 @@ This is a selfbot I wrote in around 12 hours.
 
 Copy config.example.json over to `config.json` and edit your `token` part to your token. You can get it via running `localStorage.token` in your browser. After this, you have to send your token to Lapis, who will allow you to use bots on your account. After this, install NodeJS, run `npm install`, then run `node`. Enjoy :D
 
-### You can make your own!
-
-The `CommandManager` class is only used for the `commands`, which are only in bot.js and can be ignored if you want to create your own bot with my code. 
-You can copy the ClientManager and MidiManager parts to a folder and creating your own bot.js will make them work. Those two classes are entirely self dependant.
-
 ### Todo
 
 - Clean up ClientManager (change the 30 if statements to a switch)
 - Space everything out right (everywhere is fucked)
+- make modules be able to listen on shit like `c`, like hook into into them or somehow cause I don't like that I have to add cshit in ClientManager 24/7
 
 ### Code credits
 
--- managers/ClientManager.js
+++ managers/ClientManager.js
@@ -2,6 +2,8 @@ import { EventEmitter } from "events"
 import WebSocket from "ws"
 
 import MidiManager from "./MidiManager.js"
+import BanManager from "./BanManager.js"
+
 export default class ClientManager extends EventEmitter {
 
 	constructor(url, token) {
@@ -11,7 +13,7 @@ export default class ClientManager extends EventEmitter {
 		this.token = token
 		this.users = new Map()
 		this.midi = new MidiManager(this)
-
+		this.bans = new BanManager(this)
 		this.ws = new WebSocket(this.url)
 
 		this.ws.on("open", () => {
@@ -33,7 +35,7 @@ export default class ClientManager extends EventEmitter {
 			} catch {
 				return
 			}
-			
+
 			const type = packet.m
 			
 
@@ -46,12 +48,18 @@ export default class ClientManager extends EventEmitter {
 					user: packet.p
 				})
 			} else if(type == "ch") {
-				packet.ppl.forEach(u => this.users.set(u._id, u))
+				packet.ppl.forEach(u => {
+					this.users.set(u._id, u)
+					if(this.bans.bans.includes(u._id)) 
+						this.bans.sendKickban(u._id)
+				})
 			} else if(type == "p") {
 				this.users.set(packet._id, packet)
+				if(this.bans.bans.includes(packet._id))
+					this.bans.sendKickban(packet._id)
 			} else if(type == "bye") {
 				this.users.delete(packet.ps)
-			} 
+			}
 		})
 
 
